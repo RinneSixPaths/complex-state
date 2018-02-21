b3c7ab61c33b0b5ece12f4beeed43b4c93381846
Anton Katliar <Anton_Katliar@epam.com>
2018-02-21 18:06:17 +0300

    mock shinobies data

--- src/components/Shinobies.js
+++ src/components/Shinobies.js
@@ -31,13 +31,12 @@ import Footer from './Footer/Footer';
 const pageColor = 'rgb(255, 143, 31)';
 const blankDescriptionTemplate = 'Click to view description';
 const description = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like';
-
+//сенсей: ид профиля, имя, опыт (время), текущая миссия, список студентов
 const mockMissions = [{
     id: 1,
-	rank: 'S',
-	price: '1000 $',
-	sensei: 'Yoda',
-	description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like'
+	name: 'Yoda',
+	missionsCompleted: 65,
+	student: 'Some desc'
 },
 {
     id: 2,
